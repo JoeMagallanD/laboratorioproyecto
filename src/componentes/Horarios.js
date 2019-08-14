874dcf55480fdde927aea0d929d54b24f7e25a7e
JoeMagallanD <joex.jm@gmail.co>
2019-08-14 12:22:02 -0500

    configurando el login

--- src/componentes/Horarios.js
+++ src/componentes/Horarios.js
@@ -1,10 +1,10 @@
 import React,{ Fragment} from 'react';
 import HorarioLista from './HorarioLista';
 
-function Horarios({horarios, guardarRecargarLaboratorios}){
+function Horarios({horarios, guardarRecargarLaboratorios, auth}){
     return (
         <Fragment>
-            
+            {auth ? (
                 <div>
                     <h1 className="text-center">Horarios</h1>
                     <ul className="list-group mt-5">
@@ -17,7 +17,7 @@ function Horarios({horarios, guardarRecargarLaboratorios}){
                         ))}
                     </ul>
                 </div>
-            
+            ) : <h1 className="text-center">Página no disponible</h1>}
         </Fragment>
     )
 }
